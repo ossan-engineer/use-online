ccbff50de929f8db2f5825141370da647eb25cc4
Kiichi Tachibana <bodega1123@gmail.com>
2020-10-30 17:30:27 +0900

    init create-react-hook@3.1.0

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+export const useMyHook = () => {
+  let [{ counter }, setState] = React.useState<{
+    counter: number;
+  }>({
+    counter: 0,
+  });
+
+  React.useEffect(() => {
+    let interval = window.setInterval(() => {
+      counter++;
+      setState({ counter });
+    }, 1000);
+    return () => {
+      window.clearInterval(interval);
+    };
+  }, []);
+
+  return counter;
+};
